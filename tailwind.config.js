45093b7b280bf5de5f29b2dee80284cbb0199acb
Adam Wathan <adam.wathan@gmail.com>
2020-07-23 11:02:34 -0400

    Playing with more ideas

--- tailwind.config.js
+++ tailwind.config.js
@@ -35,8 +35,11 @@ module.exports = {
           animation: {
             spin: 'spin 1s linear infinite',
             'stepped-spin': 'spin 1s steps(8) infinite',
-            pulse: 'pulse 1s cubic-bezier(0.4, 0, 1, 1) infinite',
-            fade: 'fade 2s cubic-bezier(0.4, 0, 0.2, 1) infinite',
+            pulse: 'pulse 1s cubic-bezier(0, 0, 0.2, 1) infinite',
+            // fade: 'fade 2s cubic-bezier(0.4, 0, 0.2, 1) infinite',
+            fade: 'fade 2s ease-in-out infinite',
+            // fade: 'fade 2s linear infinite',
+            wobble: 'wobble 1s ease-in-out infinite',
             'wiggle-up': 'wiggle-up 1s infinite',
             'wiggle-right': 'wiggle-right 1s infinite',
             'wiggle-down': 'wiggle-down 1s infinite',
@@ -48,14 +51,22 @@ module.exports = {
               to: { transform: 'rotate(360deg)' },
             },
             pulse: {
-              '0%': { transform: 'scale(1)', opacity: '0' },
-              '50%': { opacity: '1' },
+              '0%': { transform: 'scale(1)', opacity: '1' },
+              // '50%': { opacity: '1' },
+              '66%': { transform: 'scale(2)', opacity: '0' },
               '100%': { transform: 'scale(2)', opacity: '0' },
             },
             fade: {
               '0%': { opacity: '1' },
               '50%': { opacity: '.5' },
               '100%': { opacity: '1' },
+            },
+            wobble: {
+              // '0%': { transform: 'rotate(0deg)' },
+              '0%': { transform: 'rotate(-3deg)' },
+              '50%': { transform: 'rotate(3deg)' },
+              '100%': { transform: 'rotate(-3deg)' },
+              // '100%': { transform: 'rotate(0deg)' },
             },
             'wiggle-up': {
               '0%': {
@@ -71,12 +82,13 @@ module.exports = {
             'wiggle-right': {
               '0%': {
                 transform: 'translateX(0)',
-                animationTimingFunction: 'cubic-bezier(0.6, 0, 0.6, 1)',
+                animationTimingFunction: 'cubic-bezier(.6, 0, .6, 1)',
               },
-              '50%': {
-                transform: 'translateX(-.125rem)',
-                animationTimingFunction: 'cubic-bezier(0.4, 0, 0.4, 1)',
+              '40%': {
+                transform: 'translateX(-.25rem)',
+                animationTimingFunction: 'cubic-bezier(.4, 0, .4, 1)',
               },
+              '80%': { transform: 'translateX(0)' },
               '100%': { transform: 'translateX(0)' },
             },
             'wiggle-down': {
